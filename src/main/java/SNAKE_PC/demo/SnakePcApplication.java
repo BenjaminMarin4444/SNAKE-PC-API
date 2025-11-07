08e3277dd8db6595cbac87ef4da0f54a2a6e412b
Deybi-hud <hu4nt4r@gmail.com>
2025-11-07 00:19:51 -0300

    Update

---
+++ src/main/java/SNAKE_PC/demo/SnakePcApplication.java
@@ -0,0 +1,13 @@
+package SNAKE_PC.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakePcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakePcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SNAKE_PC/demo/SnakePcApplicationTests.java
@@ -0,0 +1,13 @@
+package SNAKE_PC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakePcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
